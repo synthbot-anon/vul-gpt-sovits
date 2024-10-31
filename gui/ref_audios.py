21752341b3494eb3e650bf31e566483ef050c544
Anonymous <>
2024-10-31 02:43:43 -0700

    Fix aux col too wide on compact mode

--- gui/ref_audios.py
+++ gui/ref_audios.py
@@ -401,7 +401,7 @@ class RefAudiosFrame(QGroupBox):
             self.table.setColumnWidth(UTTERANCE_COL, 120)
             self.table.setColumnWidth(AUDIOHASH_COL, 60)
             self.table.setColumnWidth(PRIMARY_CHECKBOX_COL, 60)
-            self.table.setColumnWidth(AUX_CHECKBOX_COL, 60)
+            self.table.setColumnWidth(AUX_CHECKBOX_COL, 40)
             self.table.setColumnWidth(PREVIEW_COL, 60)
         else:
             self.table.setColumnWidth(FILEPATH_COL, 80)
